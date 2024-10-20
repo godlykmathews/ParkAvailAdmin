755f57411e8b978ec178feb06b16332e1c4d41e8
Godly K Mathews <godlykmathews@outlook.com>
2024-10-21 00:56:03 +0530

    Update product-helper.js

--- helpers/product-helper.js
+++ helpers/product-helper.js
@@ -3,42 +3,64 @@ const collection = require('../config/collections');
 const objectId = require('mongodb').ObjectId;
 
 module.exports = {
-    addProduct: (product, callback) => {
-        console.log(product);
-
-        db.get().collection('parkingspots').insertOne(product).then((data) => {
-            console.log(data);
-            callback(data.insertedId);
-        }).catch((err) => {
-            console.error('Error occurred while adding product:', err);
-            callback(null, err);
-        });
-    },
-    getAllProducts: () => {
-        return new Promise(async (resolve, reject) => {
-            try {
-                let products = await db.get().collection(collection.PRODUCT_COLLECTION).find().toArray();
-                resolve(products);
-            } catch (err) {
-                console.error('Error occurred while fetching products:', err);
-                reject(err);
+    addProduct: (product) => {
+        return new Promise((resolve, reject) => {
+            const dbConnection = db.get();
+            if (!dbConnection) {
+                return reject(new Error('Database not connected'));
             }
+
+            dbConnection.collection(collection.PRODUCT_COLLECTION).insertOne(product)
+                .then((data) => {
+                    console.log(data);
+                    resolve(data.insertedId);
+                })
+                .catch((err) => {
+                    console.error('Error occurred while adding product:', err);
+                    reject(err);
+                });
         });
     },
+
+    getAllProducts: () => {
+        return new Promise((resolve, reject) => {
+            const dbConnection = db.get();
+            if (!dbConnection) {
+                return reject(new Error('Database not connected'));
+            }
+
+            dbConnection.collection(collection.PRODUCT_COLLECTION).find().toArray()
+                .then((products) => {
+                    resolve(products);
+                })
+                .catch((err) => {
+                    console.error('Error occurred while fetching products:', err);
+                    reject(err);
+                });
+        });
+    },
+
     deleteProduct: (placeId) => {
         return new Promise((resolve, reject) => {
-            db.get().collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: objectId(placeId) }).then((response) => {
-                if (response.deletedCount === 1) {
-                    console.log('Place deleted successfully:', response);
-                    resolve(response);
-                } else {
-                    console.error('Place not found:', response);
-                    reject(new Error('Place not found'));
-                }
-            }).catch((err) => {
-                console.error('Error occurred while deleting place:', err);
-                reject(err);
-            });
+            const dbConnection = db.get();
+            if (!dbConnection) {
+                return reject(new Error('Database not connected'));
+            }
+
+            dbConnection.collection(collection.PRODUCT_COLLECTION).deleteOne({ _id: objectId(placeId) })
+                .then((response) => {
+                    if (response.deletedCount === 1) {
+                        console.log('Place deleted successfully:', response);
+                        resolve(response);
+                    } else {
+                        console.error('Place not found:', response);
+                        reject(new Error('Place not found'));
+                    }
+                })
+                .catch((err) => {
+                    console.error('Error occurred while deleting place:', err);
+                    reject(err);
+                });
         });
     }
 };
