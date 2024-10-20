15e7554559718b40556b27d01c17a42cdfceea22
Godly K Mathews <godlykmathews@outlook.com>
2024-10-20 23:52:39 +0530

    db

--- helpers/product-helper.js
+++ helpers/product-helper.js
@@ -4,7 +4,7 @@ module.exports={
     addProduct:(product,callback)=>{
         console.log(product);
 
-        db.get().collection('product').insertOne(product).then((data)=>{
+        db.get().collection('parkingspots').insertOne(product).then((data)=>{
             console.log(data);
             callback(data.insertedId)
         })
--- routes/admin.js
+++ routes/admin.js
@@ -13,20 +13,21 @@ router.get('/', function (req, res, next) {
 
 router.get('/add-place',function(req,res){
   res.render('admin/add-place')
-})
-router.post('/add-place',(req,res)=>{
+});
 
-  productHelper.addProduct(req.body,(id)=>{
-    let image=req.files.image;
-    image.mv('./public/product-images/'+id+'.jpg',(err)=>{
-      if(!err){
-        res.render("admin/add-place");
-      }else{
+router.post('/add-place', (req, res) => {
+  productHelper.addProduct(req.body, (id) => {
+    let image = req.files.image;
+    image.mv('./public/product-images/' + id + '.jpg', (err) => {
+      if (!err) {
+        res.send('<script>alert("Successful! : New Place Added"); window.location.href = "/admin/add-place";</script>');
+      } else {
         console.log(err);
+        res.status(500).send('Error occurred while uploading image');
       }
-    })
+    });
+  });
+});
 
-  })
-})
 
 module.exports = router;
